a25f34d58d288480fbc51fd53184c75755d3395d
MdHasib01 <md.hasibuzzaman001@gmail.com>
2025-02-02 06:33:21 +0600

    cards complete

--- src/components/SportsCards.jsx
+++ src/components/SportsCards.jsx
@@ -3,6 +3,10 @@ import { FaCalendarDay } from "react-icons/fa";
 import { FaCalendarDays } from "react-icons/fa6";
 import { GiTrophy } from "react-icons/gi";
 import { FaFantasyFlightGames } from "react-icons/fa";
+import allSports from "../assets/sports/sport-all.webp";
+import cricket from "../assets/sports/cricket.webp";
+import soccer from "../assets/sports/soccer.webp";
+import tennis from "../assets/sports/tennis.webp";
 const SportsCards = () => {
   return (
     <div className="bg-blue-50">
@@ -49,7 +53,44 @@ const SportsCards = () => {
             <p className="text-xs ">Parlay</p>
           </div>
         </div>
-        <div></div>
+        <div className="w-full">
+          <div
+            style={{ backgroundImage: `url(${allSports})` }}
+            className="w-full h-[120px] bg-cover bg-center rounded-xl mb-2"
+          >
+            <div className="p-4 ">
+              <p className="text-xl font-semibold text-gray-700">All</p>
+              <p className="text-5xl font-bold">7</p>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${cricket})` }}
+            className="w-full h-[120px] bg-cover bg-center rounded-xl mb-2"
+          >
+            <div className="p-4 ">
+              <p className="text-xl font-semibold text-gray-700">Cricket</p>
+              <p className="text-5xl font-bold">3</p>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${soccer})` }}
+            className="w-full h-[120px] bg-cover bg-center rounded-xl mb-2"
+          >
+            <div className="p-4 ">
+              <p className="text-xl font-semibold text-gray-700">Soccer</p>
+              <p className="text-5xl font-bold">4</p>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${tennis})` }}
+            className="w-full h-[120px] bg-cover bg-center rounded-xl mb-2"
+          >
+            <div className="p-4 ">
+              <p className="text-xl font-semibold text-gray-700">Tennis</p>
+              <p className="text-5xl font-bold">1</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
